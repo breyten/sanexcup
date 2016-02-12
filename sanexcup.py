80902c9b0da4bff48ae8aecdc192633a89d1ab4a
Breyten Ernsting <bje@dds.nl>
2016-02-12 11:35:12 +0100

    Table output and fixing division by zero errors.

--- sanexcup.py
+++ sanexcup.py
@@ -43,10 +43,10 @@ def get_sanex_counts(team_link):
 
     soup = BeautifulSoup(html)
     #uitslagen = [s.find('span').string for s in soup.findAll('td', 'score')]
-    
+
     #for uitslag in uitslagen:
     #    if uitslag
-    
+
     for week in soup.findAll('table', 'scheduleTabs'):
         for game in week.tbody.findAll('tr'):
             home_link = game.find('td', 'teamHome').a['href']
@@ -75,7 +75,7 @@ def get_all_teams(team_id):
         html = requests.get('http://www.volleybal.nl/competitie/vereniging/%s/teams' % (team_id,)).text
     except Exception, e:
         html = None
-    
+
     if html is None:
         return []
 
@@ -88,10 +88,21 @@ def cmp_teams(a, b):
     IDX_GAMES = 1
     IDX_SANEX = 2
     IDX_ANTI_SANEX = 3
-    rate_a = a[IDX_SANEX] * 1.0 / a[IDX_GAMES]
-    rate_b = b[IDX_SANEX] * 1.0 / b[IDX_GAMES]
-    anti_rate_a = a[IDX_ANTI_SANEX] * 1.0 / a[IDX_GAMES]
-    anti_rate_b = b[IDX_ANTI_SANEX] * 1.0 / b[IDX_GAMES]
+    games_a = a[IDX_GAMES]
+    games_b = b[IDX_GAMES]
+
+    if games_a > 0:
+        rate_a = a[IDX_SANEX] * 1.0 / games_a
+        anti_rate_a = a[IDX_ANTI_SANEX] * 1.0 / games_a
+    else:
+        rate_a = 0.0
+        anti_rate_a = 0.0
+    if games_b > 0:
+        rate_b = b[IDX_SANEX] * 1.0 / games_b
+        anti_rate_b = b[IDX_ANTI_SANEX] * 1.0 / games_b
+    else:
+        rate_b = 0.0
+        anti_rate_b = 0.0
 
     if a[IDX_SANEX] == b[IDX_SANEX]:
         if rate_a == rate_b:
@@ -122,13 +133,16 @@ def main(argv=None):
         argv = sys.argv
 
     team_id = 3208 # US
+    as_html = False
 
     try:
         try:
-            opts, args = getopt.getopt(argv[1:], "hvt:", ["help", "team"])
+            opts, args = getopt.getopt(
+                argv[1:], "Hhvt:", ["help", "team", "html"]
+            )
         except getopt.error, msg:
             raise Usage(msg)
-    
+
         # option processing
         for option, value in opts:
             if option == "-v":
@@ -137,23 +151,32 @@ def main(argv=None):
                 raise Usage(help_message)
             if option in ("-o", "--output"):
                 output = value
+            if option in ("-H", "--html"):
+                as_html = True
             if option in ("-t", "--team"):
                 team_id = int(value)
-    
+
         team_links = get_all_teams(team_id)
+        pprint(team_links)
         data = {}
         for team_link in team_links:
             games, sanex, anti_sanex = get_sanex_counts(team_link)
             info = (team_link, games, sanex, anti_sanex)
             data[team_link] = info
 
+        pprint(data)
         sorted_data = sorted(data.values(), cmp=cmp_teams)
         sorted_data.reverse()
         #pprint(sorted_data)
 
         num = 1
-        print "plek\tteam naam \tgesp\t4-0\t0-4\tpct"
-        print "-"*54
+        if as_html:
+            print "<table class=\"table-striped\"><tr>"
+            print "<th>Plek</th><th>Team</th><th>Gespeeld</th>"
+            print "<th>4-0</th><th>0-4</th><th>Percentage</th></tr>"
+        else:
+            print "plek\tteam naam \tgesp\t4-0\t0-4\tpct"
+            print "-"*54
         for row in sorted_data:
             team_naam = row[0].replace(u'/competitie/team/', u'')
             matches = re.search(r'^\d+(H|D)S\+?(\d+)', team_naam)
@@ -161,9 +184,21 @@ def main(argv=None):
             if matches.group(1) == 'D':
                 kind = "Dames"
             team_naam = u"%s %s" % (kind, matches.group(2))
-            ratio = row[2] * 100.0 / row[1]
-            print "%d\t%s \t%s\t%d\t%d\t%.2f" % (num, team_naam, row[1], row[2], row[3], ratio,)
+            if row[1] > 0:
+                ratio = row[2] * 100.0 / row[1]
+            else:
+                ratio = 0.0
+            if as_html:
+                print "<tr><td>%d</td><td>%s</td><td>%s</td><td>%d</td><td>%d</td><td>%.2f</td></tr>" % (
+                    num, team_naam, row[1], row[2], row[3], ratio,
+                )
+            else:
+                print "%d\t%s \t%s\t%d\t%d\t%.2f" % (
+                    num, team_naam, row[1], row[2], row[3], ratio,
+                )
             num += 1
+        if as_html:
+            print "</table>"
 
     except Usage, err:
         print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
